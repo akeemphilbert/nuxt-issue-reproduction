2bc943b788b28a5c93adacdf1a0c3c9cf3be71d7
akeemphilbert <akeem.philbert@wepala.com>
2022-07-27 06:07:17 -0400

    1 file modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,10 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-        api1Base: '/api',
-    }
-}
-
-})
+      api1Base: '/api',
+    },
+  },
+});
